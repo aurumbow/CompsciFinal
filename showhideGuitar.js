1432684e5bade08d57222cc513a5279c308f4e7f
aurumbow <45271891+aurumbow@users.noreply.github.com>
2018-12-04 14:51:53 -0500

    Create showhideGuitar.js

---
+++ showhideGuitar.js
@@ -0,0 +1,10 @@
+function showhideGuitar(){
+  var x = document.getElementById("guitar");
+   if (x.style.display === "none") {
+       x.style.display = "block";
+   } else {
+       x.style.display = "none";
+   }
+}
+
+//code credit: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
